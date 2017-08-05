4ddb7ed6e780f5f85a297b651519f223561d62c2
Vinay S Shenoy <vinaysshenoy@gmail.com>
2017-08-05 15:15:11 +0530

    Initial commit

---
+++ app/src/main/java/com/vinaysshenoy/multitouch/widget/playground/PlaygroundView.java
@@ -0,0 +1,28 @@
+package com.vinaysshenoy.multitouch.widget.playground;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by vinaysshenoy on 05/08/17.
+ */
+
+public class PlaygroundView extends View {
+    public PlaygroundView(Context context) {
+        super(context);
+    }
+
+    public PlaygroundView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PlaygroundView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public PlaygroundView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
