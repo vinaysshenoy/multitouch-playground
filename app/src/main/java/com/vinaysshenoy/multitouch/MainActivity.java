60d6992483ccac826730e88d9f979faad05d674a
Vinay S Shenoy <vinaysshenoy@gmail.com>
2017-08-05 15:49:36 +0530

    Add items to PlaygroundView

--- app/src/main/java/com/vinaysshenoy/multitouch/MainActivity.java
+++ app/src/main/java/com/vinaysshenoy/multitouch/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.vinaysshenoy.multitouch.widget.playground.PlaygroundView;
+import com.vinaysshenoy.multitouch.widget.playground.shapes.Circle;
+import com.vinaysshenoy.multitouch.widget.playground.shapes.Square;
+import com.vinaysshenoy.multitouch.widget.playground.shapes.Triangle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,23 +20,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        playgroundView = (PlaygroundView) findViewById(R.id.playground);
         controlPanel = findViewById(R.id.controlPanel);
         controlPanel.findViewById(R.id.btn_circle).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                playgroundView.addShape(new Circle(64F));
             }
         });
         controlPanel.findViewById(R.id.btn_square).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                playgroundView.addShape(new Square(48F));
             }
         });
         controlPanel.findViewById(R.id.btn_triangle).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                playgroundView.addShape(new Triangle(72F));
             }
         });
     }
